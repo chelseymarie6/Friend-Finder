13e0073d9d09a50525995388f80a62b65c7e2da6
Chelsey Dolan <chelseymarie6@gmail.com>
2017-11-03 23:35:38 -0700

    updates

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,28 +1,28 @@
 var friendData = [
 	{
-	"name":"Lucas",
-	"photo":"https://pre00.deviantart.net/09d6/th/pre/i/2016/211/e/5/stranger_things___lucas_by_ruizburgos-daby4bp.jpg",
-	"scores":[4, 5,	5, 3, 2, 3, 1, 1, 2, 2]
+	name:"Lucas",
+	photo:"https://pre00.deviantart.net/09d6/th/pre/i/2016/211/e/5/stranger_things___lucas_by_ruizburgos-daby4bp.jpg",
+	scores:[4, 5, 5, 3, 2, 3, 1, 1, 2, 2]
 	},
 	{
-	"name":"Will",
-	"photo":"https://img00.deviantart.net/d292/i/2016/217/e/e/stranger_things___will_by_ruizburgos-daclut7.jpg",
-	"scores":[3, 1, 2, 5, 2, 4, 1, 2, 4, 4]
+	name:"Will",
+	photo:"https://img00.deviantart.net/d292/i/2016/217/e/e/stranger_things___will_by_ruizburgos-daclut7.jpg",
+	scores:[3, 1, 2, 5, 2, 4, 1, 2, 4, 4]
 	},
 	{
-	"name":"Mike",
-	"photo":"https://img00.deviantart.net/8550/i/2016/215/1/0/stranger_things___mike_by_ruizburgos-dacfule.jpg",
-	"scores":[4, 5, 5, 3, 2, 3, 1, 1, 2, 2]
+	name:"Mike",
+	photo:"https://img00.deviantart.net/8550/i/2016/215/1/0/stranger_things___mike_by_ruizburgos-dacfule.jpg",
+	scores:[4, 5, 5, 3, 2, 3, 1, 1, 2, 2]
 	},
 	{
-	"name":"Eleven",
-	"photo":"https://img00.deviantart.net/6333/i/2016/217/0/6/stranger_things___eleven_by_ruizburgos-dacopuh.jpg",
-	"scores":[4, 5, 5, 3, 2, 3, 1, 1, 2, 2]
+	name:"Eleven",
+	photo:"https://img00.deviantart.net/6333/i/2016/217/0/6/stranger_things___eleven_by_ruizburgos-dacopuh.jpg",
+	score:[4, 5, 5, 3, 2, 3, 1, 1, 2, 2]
 	},
 	{
-	"name":"Dustin",
-	"photo":"https://static1.squarespace.com/static/51b3dc8ee4b051b96ceb10de/t/57ab627d8419c2acafa2f7ca/1470849667937/wonderfull-retro-stranger-things-character-portraits-by-ruiz-burgos1",
-	"scores":[4, 5, 5, 3, 2, 3,	1, 1, 2, 2]
+	name:"Dustin",
+	photo:"https://img00.deviantart.net/a0bf/i/2016/209/f/f/stranger_things___dustin_by_ruizburgos-dabopon.jpg",
+	scores:[4, 5, 5, 3, 2, 3, 1, 1, 2, 2]
 	}
 ];
 
